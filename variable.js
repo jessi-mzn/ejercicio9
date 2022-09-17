2118e765e2f18509b53e3068fabd5426404d6bff
jesica <113074000+jessi-mzn@users.noreply.github.com>
2022-09-17 12:19:03 -0300

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,14 @@
+
+//VARIABLES
+//Una variable es un contenedor de informacion
+
+var pais = "Argentina";
+var continente = "Latinoamerica"
+var antiguedad = "2021"
+var pais_y_continente = pais + " " + continente;
+
+pais = "España";
+continente = "Europa";
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
